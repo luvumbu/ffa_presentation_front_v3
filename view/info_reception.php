4f6a79333c2cb5c01ef4cfbcbaa9574d75374e0e
luvumbu <luvumbu.n@gmail.com>
2022-03-10 16:28:43 +0100

    data

--- view/info_reception.php
+++ view/info_reception.php
@@ -22,7 +22,7 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
-    echo "<a href='".$row['get_users_nom_complet'].">". $row["get_users_nom_complet"].'</a><br/>' ;
+    echo "<a href='test ok'>". $row["get_users_nom_complet"].'</a><br/>' ;
   }
 } else {
   echo "0 results";
