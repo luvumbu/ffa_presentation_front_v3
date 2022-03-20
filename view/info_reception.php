3eba72be3ea0d0429b5f09056131afce3c1fdf48
luvumbu <luvumbu.n@gmail.com>
2022-03-20 14:41:36 +0100

    Mise a jour des docs ok local et en ligne

--- view/info_reception.php
+++ view/info_reception.php
@@ -22,8 +22,49 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
-    echo "<div class='ok' onclick='ok_()'>". $row["get_users_nom_complet"].'</div><br/>' ;
-    echo "resultat ok" ; 
+    // debut des condition 
+  
+    
+
+
+    switch ($type) {
+      case "get_club_nom_complet":
+        echo "NICE" ;
+        echo "<div class='ok' onclick='ok_()'>". $row["get_users_nom_complet"].'</div><br/>' ;
+        break;
+      case "get_users_nom_complet":
+
+
+        echo "<div class='ok' onclick='ok_()'>". $row["epreuve_sex"].'</div><br/>' ;
+        echo "<div class='ok' onclick='ok_()'>". $row["get_cat"].'</div><br/>' ;
+        echo "<div class='ok' onclick='ok_()'>". $row["get_club_departement"].'</div><br/>' ;
+        echo "<div class='ok' onclick='ok_()'>". $row["get_club_nom_complet"].'</div><br/>' ;
+        echo "<div class='ok' onclick='ok_()'>". $row["get_club_region"].'</div><br/>' ;
+        echo "<div class='ok' onclick='ok_()'>". $row["get_epreuve_emplacement"].'</div><br/>' ;
+        echo "<div class='ok' onclick='ok_()'>". $row["get_result_users_nom_0"].'</div><br/>' ;
+        echo "<div class='ok' onclick='ok_()'>". $row["get_epreuve_nom_complet"].'</div><br/>' ;
+        echo "<div class='ok' onclick='ok_()'>". $row["get_result_date_perf"].'</div><br/>' ;
+        echo "<div class='ok' onclick='ok_()'>". $row["get_result_users_nom_1"].'</div><br/>' ;
+
+        echo "<div class='ok' onclick='ok_()'>". $row["get_result_users_nom_2"].'</div><br/>' ;
+        echo "<div class='ok' onclick='ok_()'>". $row["get_result_users_perf"].'</div><br/>' ;
+        echo "<div class='ok' onclick='ok_()'>". $row["get_result_users_perf2"].'</div><br/>' ;
+        echo "<div class='ok' onclick='ok_()'>". $row["get_result_villes_nom"].'</div><br/>' ;
+        break; 
+
+        case "get_result_villes_nom":
+         echo "<div class='ok' onclick='ok_()'>". $row["get_users_nom_complet"].'</div><br/>' ;
+      
+         break; 
+
+
+
+
+        
+      default:
+        echo "Your favorite color is neither red, blue, nor green!";
+    }
+    // fin des conditions 
   }
 } else {
   echo "0 results";
